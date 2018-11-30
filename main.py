50928493f1d0154e3c14c91095912d9798eabbcd
lotalorafox <lotalorafox@outlook.com>
2018-11-29 23:10:26 -0500

    little change to show just in the end

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     spiderform = open("./forms/spider.form","r")
     spiderspoints = spiderform.readlines()
     #define world
-    win = GraphWin("Spider and Flyes",wight,height)
+    
     spiders = list()
     flies = list()
     #put init
@@ -33,7 +33,7 @@ def main():
         s+=1
         spy.append(len(spiders))
         fli.append(len(flies))
-        clear(win)
+        # clear(win)
         # drawspiders(spiders,win,spiderspoints)    
         # drawflies(flies,win,flypoints)
         moveflies(flies)
@@ -52,6 +52,7 @@ def main():
             else:
                 break   
         # time.sleep(.01)
+    win = GraphWin("Spider and Flyes",wight,height)
     series.append(s)
     s+=1
     spy.append(len(spiders))
@@ -59,15 +60,15 @@ def main():
     print(spy)
     print(fli)
     # plt.subplot(211)
-    
-    # draw final
-    clear(win)
-    drawflies(flies,win,flypoints)
-    drawspiders(spiders,win,spiderspoints)    
+#     plot
     plt.plot(series,fli,label="Flies")
     plt.plot(series,spy,label="Spiders")
     plt.legend(bbox_to_anchor=(.8,1),loc=2,borderaxespad=0 )
     plt.show()
+    # draw final
+    clear(win)
+    drawflies(flies,win,flypoints)
+    drawspiders(spiders,win,spiderspoints)    
     win.getMouse()
     win.close()
 def clear(window):
